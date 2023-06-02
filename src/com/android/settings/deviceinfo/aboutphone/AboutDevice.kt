cf47c7c748e7de4b99212507465e0f06266a0462
RealLanta <lanta@bangumi.cyou>
2023-06-02 08:51:35 +0800

    AboutDevice.kt: Remove extra version infomation

--- src/com/android/settings/deviceinfo/aboutphone/AboutDevice.kt
+++ src/com/android/settings/deviceinfo/aboutphone/AboutDevice.kt
@@ -28,7 +28,7 @@ class AboutDevice : FrameLayout {
         val releasetype = SystemProperties.get("ro.lineage.releasetype")
         val version = SystemProperties.get("ro.lineage.version")
 
-         findViewById<TextView>(R.id.romVersion).text = (version + " " + releasetype).toString()
+         findViewById<TextView>(R.id.romVersion).text = (version).toString()
 
         // Device
         var mDeviceName = Settings.Global.getString(
